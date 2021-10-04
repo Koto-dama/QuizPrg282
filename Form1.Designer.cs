cb4da5ccefd87fd2597e2c41059f85c39af84732
Koto-dama <muhammadabam@gmail.com>
2021-10-04 15:33:16 +0200

    Created Text File, added button and listbox

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,46 @@ namespace QuizPrg282
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.lbMain = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(293, 354);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(188, 58);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "Read from the text file";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // lbMain
+            // 
+            this.lbMain.FormattingEnabled = true;
+            this.lbMain.Location = new System.Drawing.Point(42, 12);
+            this.lbMain.Name = "lbMain";
+            this.lbMain.Size = new System.Drawing.Size(722, 316);
+            this.lbMain.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbMain);
+            this.Controls.Add(this.btnRead);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.ListBox lbMain;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,15 @@ namespace QuizPrg282
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
